c66e1dba546e18adcd76c699f7dc9c9697443fba
Liam Bowers <liamondrop@gmail.com>
2020-04-27 15:08:18 -0700

    updates and improvements for blog post

---
+++ Assets/ReachTarget/Scripts/ReachTargetAgent.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SimpleVehicleController))]
+public class ReachTargetAgent : Agent
+{
+    public Transform target;
+    public Renderer ground;
+    private Rigidbody m_rbody;
+    private SimpleVehicleController m_controller;
+    private float m_rewardScalar = 0.0001f;
+    private bool m_targetReached = false;
+
+    protected Vector3 AgentVelocity
+    {
+        get { return m_rbody.velocity; }
+    }
+
+    protected float RewardScalar
+    {
+        get { return m_rewardScalar; }
+    }
+
+    void Start()
+    {
+        m_rbody = GetComponent<Rigidbody>();
+        m_controller = GetComponent<SimpleVehicleController>();
+
+        // If the agent's maxStep > 0, use it to calculate the
+        // reward scalar. To be used for per-step rewards
+        if (maxStep > 0)
+        {
+            m_rewardScalar = 1f / maxStep;
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        var distanceToTarget = Vector3.Distance(
+            this.transform.position,
+            target.transform.position
+        );
+
+        // Upon reaching the target, respawn it to a random position
+        // and add reward of +1
+        if (distanceToTarget < 1)
+        {
+            m_targetReached = true;
+            AddReward(1f);
+            EndEpisode();
+        }
+
+        // If the car falls off the platform, end episode
+        if (this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // Only respawn the agent if we didn't reach the target
+        if (!m_targetReached)
+            RespawnAgent();
+        m_targetReached = false;
+        RespawnTarget();
+    }
+
+    public override void OnActionReceived(float[] action)
+    {
+        m_controller.Steering = action[0];
+        m_controller.Throttle = action[1];
+        m_controller.Brake = action[2];
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EndEpisode();
+    }
+
+    public override float[] Heuristic()
+    {
+        var action = new float[3];
+        action[0] = Input.GetAxis("Horizontal");
+        action[1] = Input.GetAxis("Vertical");
+        action[2] = Input.GetAxis("Jump");
+        return action;
+    }
+
+    void RespawnAgent()
+    {
+        m_controller.Throttle = 0;
+        m_controller.Steering = 0;
+        m_controller.Brake = 1;
+        m_rbody.velocity = Vector3.zero;
+        m_rbody.angularVelocity = Vector3.zero;
+        this.transform.localPosition = new Vector3(0, 1f, 0);
+        this.transform.rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
+    }
+
+    void RespawnTarget()
+    {
+        Vector3 extents = ground.bounds.extents - (Vector3.one * 3);
+        target.localPosition = new Vector3(Random.Range(-extents.x, extents.x),
+                                           0.5f,
+                                           Random.Range(-extents.z, extents.z));
+        target.rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
+    }
+}
--- Assets/ReachTarget/Scripts/ReachTargetAgent_0.cs
+++ Assets/ReachTarget/Scripts/ReachTargetAgent_0.cs
@@ -4,94 +4,15 @@ using UnityEngine;
 using MLAgents;
 using MLAgents.Sensors;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(SimpleVehicleController))]
-public class ReachTargetAgent_0 : Agent
+public class ReachTargetAgent_0 : ReachTargetAgent
 {
-    public Transform target;
-    public Renderer ground;
-    private Rigidbody m_rbody;
-    private SimpleVehicleController m_controller;
-
-    void Start()
-    {
-        m_rbody = GetComponent<Rigidbody>();
-        m_controller = GetComponent<SimpleVehicleController>();
-    }
-
     public override void CollectObservations(VectorSensor sensor)
     {
         // Target and agent positions
         sensor.AddObservation(target.position); // vec 3
-        sensor.AddObservation(gameObject.transform.position); // vec 3
+        sensor.AddObservation(this.transform.position); // vec 3
 
         // Agent velocity
-        sensor.AddObservation(m_rbody.velocity); // vec 3
-
-        var distanceToTarget = Vector3.Distance(
-            gameObject.transform.position,
-            target.transform.position
-        );
-
-        // Upon reaching the target, respawn it to a random position
-        // and add reward of +1
-        if (distanceToTarget < 1)
-        {
-            AddReward(1f);
-            RespawnTarget();
-        }
-
-        // If the car falls off the platform, end episode
-        if (gameObject.transform.localPosition.y < 0)
-        {
-            EndEpisode();
-        }
-    }
-
-    public override void OnEpisodeBegin()
-    {
-        RespawnTarget();
-        RespawnAgent();
-    }
-
-    public override void OnActionReceived(float[] action)
-    {
-        m_controller.Steering = action[0];
-        m_controller.Throttle = action[1];
-        m_controller.Brake = action[2];
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        EndEpisode();
-    }
-
-    public override float[] Heuristic()
-    {
-        var action = new float[3];
-        action[0] = Input.GetAxis("Horizontal");
-        action[1] = Input.GetAxis("Vertical");
-        action[2] = Input.GetAxis("Jump");
-        return action;
-    }
-
-    void RespawnAgent()
-    {
-        m_controller.Throttle = 0;
-        m_controller.Steering = 0;
-        m_controller.Brake = 1;
-        m_rbody.velocity = Vector3.zero;
-        m_rbody.angularVelocity = Vector3.zero;
-        gameObject.transform.localPosition = new Vector3(0, 1f, 0);
-        gameObject.transform.rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
-    }
-
-    void RespawnTarget()
-    {
-        Vector3 extents = ground.bounds.extents - (Vector3.one * 3);
-        target.localPosition = new Vector3(Random.Range(-extents.x, extents.x),
-                                           0.5f,
-                                           Random.Range(-extents.z, extents.z));
-        target.rotation = Quaternion.Euler(0, Random.Range(-180, 180), 0);
+        sensor.AddObservation(AgentVelocity); // vec 3
     }
 }
